00093153ab0ba1fc0f3d83688f8806c8068b367c
LACH <laurachg8@gmail.com>
2018-11-17 11:32:36 -0600

    Database integrated

--- logic.js
+++ logic.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
 
-    $("#add-employee").click(function (e) {
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyAnkPIzEiHJ6Qd6-lhbBFFtrbp4A8K1urI",
+    authDomain: "timesheet-9e7c0.firebaseapp.com",
+    databaseURL: "https://timesheet-9e7c0.firebaseio.com",
+    projectId: "timesheet-9e7c0",
+    storageBucket: "",
+    messagingSenderId: "653254569801"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+ 
+
+
+    $("#submitBtn").click(function (e) {
 
         e.preventDefault();
     
@@ -10,12 +27,11 @@ $(document).ready(function () {
         var datedata = $("<td>");
         var ratedata = $("<td>");
     
-        var name = $("#name").val().trim();
-        var role = $("#role").val().trim();
-        var date = $("#date").val().trim();
-        var rate = $("#rate").val().trim();
-    
-        console.log(name);
+        var name = $("#name-input").val().trim();
+        var role = $("#role-input").val().trim();
+        var date = $("#date-input").val().trim();
+        var rate = $("#rate-input").val().trim();
+
         namedata.text(name);
         roledata.text(role);
         datedata.text(date);
@@ -24,22 +40,19 @@ $(document).ready(function () {
         row.append(namedata, roledata, datedata, ratedata);
     
         $("#employee-table > tbody").append(row);
+
+        database.ref().push({
+            name: name,
+            role: role,
+            date: date,
+            rate: rate
+        })
     
     });
 
-    <script src="https://www.gstatic.com/firebasejs/5.5.8/firebase.js"></script>
-        <script>
-            // Initialize Firebase
-            var config = {
-                apiKey: "AIzaSyAnkPIzEiHJ6Qd6-lhbBFFtrbp4A8K1urI",
-            authDomain: "timesheet-9e7c0.firebaseapp.com",
-            databaseURL: "https://timesheet-9e7c0.firebaseio.com",
-            projectId: "timesheet-9e7c0",
-            storageBucket: "",
-            messagingSenderId: "653254569801"
-          };
-          firebase.initializeApp(config);
-</script>
+           
+
+
 })
 
 
